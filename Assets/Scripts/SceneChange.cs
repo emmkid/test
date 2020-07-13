d267316fc583e34127d4f6b7b07496631e0aecd7
emma <emmakidwell@yahoo.com>
2020-07-13 19:59:45 -0400

    Just doing a thing

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+ //Once the gameobject tagged as player collides with the trigger, which is just tagged as the default other, the game checks to see if the gameobject it collided with was tagged as player. If 
+ //yes, it runs the code below which takes the player to the next scene, marked Exit. Make sure the scene is in your build settings. 
+        {
+        if (other.gameObject.tag == "Player")
+            SceneManager.LoadScene("Exit");
+        }
+ }
+   
+
